5dec37728110da573c923d0b7dd003826a599be3
Parot Ketpong <parot@mtrgroup.local>
2023-09-28 23:04:50 +0700

    Fixed : Exercise Equality of values

--- ex-3.js
+++ ex-3.js
@@ -1,13 +1,27 @@
 function isPlainObject(value) {
-  // Start coding here
-}
+  let result ;
+  if(value === Object){
+      result = true;
+  }else if (value === Array) {
+      result = false
+  }else if (value === null) {
+      result = false
+  }else{
+      result = "This is not an object"
+  }
+  return result
+  }
+  
+    // Example case
+    let result1 = isPlainObject({ name: "John" });
+    let result2 = isPlainObject(null);
+    let result3 = isPlainObject(["apples", "oranges"]);
+    
+    // ผลลัพธ์ควรที่ได้จาก Example case
+    console.log(result1); //true
+    console.log(result2); // false
+    console.log(result3); // false
+    console.log(typeof(result3))
 
-// Example case
-let result1 = isPlainObject({ name: "John" });
-let result2 = isPlainObject(null);
-let result3 = isPlainObject(["apples", "oranges"]);
-
-// ผลลัพธ์ควรที่ได้จาก Example case
-console.log(result1); //true
-console.log(result2); // false
-console.log(result3); // false
+    let test = ["apples", "oranges"]
+  console.log(Array.isArray(test))
